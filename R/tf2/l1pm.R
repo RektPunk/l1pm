6bacb1374e4ca02dd928ab64775d90c1bcb8e1f8
RektPunk <110188257+RektPunk@users.noreply.github.com>
2024-07-26 13:05:34 +0900

    Update l1pm.R

--- R/tf2/l1pm.R
+++ R/tf2/l1pm.R
@@ -1,23 +1,23 @@
 library(tensorflow)
 library(keras3)
 
-l1_p <- function(X, y, test_X, valid_X, tau, hidden_dim1, hidden_dim2, learning_rate, max_deep_iter, lambda_obj, penalty = 0) {
-  input_dim <- ncol(X)
-  n <- nrow(X)
-  r <- length(tau)
-  p <- hidden_dim2 + 1
-  tau_mat <- matrix(rep(tau, each = n), ncol = 1)
+l1_p = function(X, y, test_X, valid_X, tau, hidden_dim1, hidden_dim2, learning_rate, max_deep_iter, lambda_obj, penalty = 0) {
+  input_dim = ncol(X)
+  n = nrow(X)
+  r = length(tau)
+  p = hidden_dim2 + 1
+  tau_mat = matrix(rep(tau, each = n), ncol = 1)
   
-  input_x <- layer_input(shape = input_dim)
-  output_y <- tf$constant(y, dtype = tf$float32)
-  output_y_tiled <- tf$tile(output_y, multiples = tf$constant(as.integer(c(r, 1)), dtype = tf$int32))
-  tau_tf <- tf$constant(tau_mat, dtype = tf$float32)
+  input_x = layer_input(shape = input_dim)
+  output_y = tf$constant(y, dtype = tf$float32)
+  output_y_tiled = tf$tile(output_y, multiples = tf$constant(as.integer(c(r, 1)), dtype = tf$int32))
+  tau_tf = tf$constant(tau_mat, dtype = tf$float32)
   
   # Layer 1
-  hidden_layer_1 <- layer_dense(units = hidden_dim1, activation = "sigmoid", kernel_initializer = initializer_random_normal())
+  hidden_layer_1 = layer_dense(units = hidden_dim1, activation = "sigmoid", kernel_initializer = initializer_random_normal())
   
   # Layer 2
-  hidden_layer_2 <- layer_dense(units = hidden_dim2, activation = "sigmoid", kernel_initializer = initializer_random_normal())
+  hidden_layer_2 = layer_dense(units = hidden_dim2, activation = "sigmoid", kernel_initializer = initializer_random_normal())
   
   # Output layer
   outputLayer = Layer(
@@ -39,21 +39,21 @@ l1_p <- function(X, y, test_X, valid_X, tau, hidden_dim1, hidden_dim2, learning_
     },
     call = function(inputs) {
       delta_mat = tf$concat(list(self$delta_0_mat, self$delta_coef_mat), axis = 0L)
-      beta_mat <- tf$transpose(tf$cumsum(tf$transpose(delta_mat)))
+      beta_mat = tf$transpose(tf$cumsum(tf$transpose(delta_mat)))
       
-      delta_vec <- delta_mat[2:(self$input_dim + 1), 2:self$units]
-      self$delta_0_vec <- delta_mat[1, 2:self$units, drop = FALSE]
-      delta_minus_vec <- tf$maximum(0, -delta_vec)
-      delta_minus_vec_sum <- tf$reduce_sum(delta_minus_vec, axis = 0L)
-      self$delta_0_vec_clipped <- tf$clip_by_value(
+      delta_vec = delta_mat[2:(self$input_dim + 1), 2:self$units]
+      self$delta_0_vec = delta_mat[1, 2:self$units, drop = FALSE]
+      delta_minus_vec = tf$maximum(0, -delta_vec)
+      delta_minus_vec_sum = tf$reduce_sum(delta_minus_vec, axis = 0L)
+      self$delta_0_vec_clipped = tf$clip_by_value(
         self$delta_0_vec,
         clip_value_min = delta_minus_vec_sum,
         clip_value_max = tf$constant(Inf, shape = delta_minus_vec_sum$shape, dtype = tf$float32)
       )
-      predicted_y_modified <- tf$matmul(inputs, beta_mat[2:(self$input_dim + 1), ]) +
+      predicted_y_modified = tf$matmul(inputs, beta_mat[2:(self$input_dim + 1), ]) +
         tf$cumsum(tf$concat(list(beta_mat[1, 1, drop = FALSE], self$delta_0_vec_clipped), axis = 1L), axis = 1L)
-      predicted_y <- tf$matmul(inputs, beta_mat[2:(self$input_dim + 1), ]) + beta_mat[1, ]
-      predicted_y_tiled <- tf$reshape(tf$transpose(predicted_y), shape = tf$constant(as.integer(c(-1L, 1)), dtype = tf$int32))
+      predicted_y = tf$matmul(inputs, beta_mat[2:(self$input_dim + 1), ]) + beta_mat[1, ]
+      predicted_y_tiled = tf$reshape(tf$transpose(predicted_y), shape = tf$constant(as.integer(c(-1L, 1)), dtype = tf$int32))
       return(predicted_y_tiled)
     }
   )
@@ -62,22 +62,22 @@ l1_p <- function(X, y, test_X, valid_X, tau, hidden_dim1, hidden_dim2, learning_
   model %>% hidden_layer_1 %>% hidden_layer_2 %>% output_layer
   
   quantile_loss = function(output_y, predicted_y_tiled) {
-    output_y_tiled <- tf$tile(output_y, multiples = tf$constant(as.integer(c(r, 1)), dtype = tf$int32))
-    diff_y <- output_y_tiled - predicted_y_tiled
+    output_y_tiled = tf$tile(output_y, multiples = tf$constant(as.integer(c(r, 1)), dtype = tf$int32))
+    diff_y = output_y_tiled - predicted_y_tiled
     loss = tf$reduce_mean(diff_y * (tau_tf - (tf$sign(-diff_y) + 1) / 2))
 
     delta_mat = tf$concat(list(output_layer$delta_0_mat, output_layer$delta_coef_mat), axis = 0L)
-    beta_mat <- tf$transpose(tf$cumsum(tf$transpose(delta_mat)))
-    delta_vec <- delta_mat[2:p, 2:r]
-    delta_0_vec <- delta_mat[1, 2:r, drop = FALSE]
-    delta_minus_vec <- tf$maximum(0, -delta_vec)
-    delta_minus_vec_sum <- tf$reduce_sum(delta_minus_vec, axis = 0L)
-    delta_0_vec_clipped <- tf$clip_by_value(
+    beta_mat = tf$transpose(tf$cumsum(tf$transpose(delta_mat)))
+    delta_vec = delta_mat[2:p, 2:r]
+    delta_0_vec = delta_mat[1, 2:r, drop = FALSE]
+    delta_minus_vec = tf$maximum(0, -delta_vec)
+    delta_minus_vec_sum = tf$reduce_sum(delta_minus_vec, axis = 0L)
+    delta_0_vec_clipped = tf$clip_by_value(
       delta_0_vec,
       clip_value_min = delta_minus_vec_sum,
       clip_value_max = tf$constant(Inf, shape = delta_minus_vec_sum$shape, dtype = tf$float32))
 
-    res <-  loss +
+    res =  loss +
       penalty * (
         tf$reduce_mean(tf$square(hidden_layer_1$kernel)) + 
         tf$reduce_mean(tf$square(hidden_layer_2$kernel)) +
@@ -87,15 +87,15 @@ l1_p <- function(X, y, test_X, valid_X, tau, hidden_dim1, hidden_dim2, learning_
     return(loss)
   }
 
-  optimizer <- optimizer_rmsprop(learning_rate = learning_rate)
+  optimizer = optimizer_rmsprop(learning_rate = learning_rate)
   model = model %>% compile(loss = quantile_loss, optimizer = optimizer)
   model %>% fit(X, output_y, epochs = max_deep_iter, batch_size = n)
   
-  y_predict <- model %>% predict(X, batch_size = n)
-  y_valid_predict <- model %>% predict(valid_X, batch_size = nrow(valid_X))
-  y_test_predict <- model %>% predict(test_X, batch_size = nrow(test_X))
+  y_predict = model %>% predict(X, batch_size = n)
+  y_valid_predict = model %>% predict(valid_X, batch_size = nrow(valid_X))
+  y_test_predict = model %>% predict(test_X, batch_size = nrow(test_X))
   
-  barrier_result <- list(
+  barrier_result = list(
     y_predict = matrix(y_predict, n, r), 
     y_valid_predict = matrix(y_valid_predict, nrow(valid_X), r),
     y_test_predict = matrix(y_test_predict, nrow(test_X), r)
